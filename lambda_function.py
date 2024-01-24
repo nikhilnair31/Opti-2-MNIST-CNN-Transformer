108eea490c4bc3c5dcb912dbf57733ff32f93a36
Nikhil Nair <niknair31898@gmail.com>
2024-01-24 16:20:56 -0600

    aws deploy

--- lambda_function.py
+++ lambda_function.py
@@ -45,8 +45,7 @@ def handler(event, context):
         predicted_label = labels[predicted_label_index]
 
         # Load the original label
-        original_label_index = int(body['label'])
-        original_label = labels[original_label_index]
+        original_label = body['label']
 
         # Return the result
         return {
