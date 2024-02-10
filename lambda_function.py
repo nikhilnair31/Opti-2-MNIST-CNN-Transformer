548d314905749f737b0fbbe53d6ae395aaebdc2f
Nikhil Nair <niknair31898@gmail.com>
2024-02-09 18:32:00 -0600

    aws deploy

--- lambda_function.py
+++ lambda_function.py
@@ -27,6 +27,28 @@ s3.download_file(bucket_name, trasformer_model_key, trasformer_model_path)
 cnn_model = tf.keras.models.load_model(cnn_model_path)
 trasformer_model = tf.keras.models.load_model(trasformer_model_path)
 
+def predict(image_data):
+    # Use the cnn_model to classify the image
+    cnn_prediction = cnn_model.predict(image_data)
+    cnn_predicted_class = int(np.argmax(cnn_prediction))
+    # Use the cnn_model to classify the image
+    trasformer_prediction = trasformer_model.predict(image_data)
+    trasformer_predicted_class = int(np.argmax(trasformer_prediction))
+
+    return cnn_predicted_class, trasformer_predicted_class
+
+def to_image(image_data):
+    # Convert the image data to bytes
+    img = Image.fromarray((image_data * 255).astype(np.uint8))
+    img_byte_array = io.BytesIO()
+    img.save(img_byte_array, format='PNG')
+    img_byte_array = img_byte_array.getvalue()
+
+    # Encode the image data in base64
+    base64_img = base64.b64encode(img_byte_array).decode('utf-8')
+
+    return base64_img
+
 def handler(event, context):
     try:
         logger.info(f"event: {event}")
@@ -37,30 +59,32 @@ def handler(event, context):
         # Retrieve CSV data from the event
         csv_data_base64 = body.get('csv_data', '')
         csv_data = base64.b64decode(csv_data_base64).decode('utf-8')
-
+        
         # Load CSV data into a DataFrame
-        data = pd.read_csv(io.StringIO(csv_data), header=None)
-
+        csv_df = pd.read_csv(io.StringIO(csv_data), header=None)
+        
         # Convert the DataFrame to a NumPy array
-        image = data.to_numpy()
+        image_data = csv_df.to_numpy()
 
         # Check if normalization is needed (assuming the values are either in 0-255 or 0-1 range)
-        if image.max() > 1.0:
+        if image_data.max() > 1.0:
             # Normalize pixel values to be in the range [0.0, 1.0] if they're in the 0-255 range
-            image = image / 255.0
+            image_data = image_data / 255.0
 
-        # Use the cnn_model to classify the image
-        cnn_prediction = cnn_model.predict(image_data)
-        cnn_predicted_class = int(np.argmax(cnn_prediction))
-        # Use the cnn_model to classify the image
-        trasformer_prediction = trasformer_model.predict(image_data)
-        trasformer_predicted_class = int(np.argmax(trasformer_prediction))
-
-        # Return the result
-        return {
-            'cnn_predicted_label': cnn_predicted_class,
-            'transformer_predicted_label': trasformer_predicted_class
-        }
+        function = body.get('function', '')
+        if function == 'predict':
+            cnn_predicted_class, transformer_predicted_class = predict(image_data)
+            return {
+                'cnn_predicted_label': cnn_predicted_class,
+                'transformer_predicted_label': transformer_predicted_class
+            }
+        elif function == 'to_image':
+            base64_img = to_image(image_data)
+            return {
+                'base64_img': base64.b64encode(base64_img).decode('utf-8')
+            }
+        else:
+            return {"error": "Invalid function specified"}
 
     except Exception as e:
         logger.error(f"Error: {str(e)}")
